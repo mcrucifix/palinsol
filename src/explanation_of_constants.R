23187a57c792943ae2adb8587ef9febc4349cc98
mcrucifix <michel.crucifix@uclouvain.be>
2024-03-19 16:35:06 +0100

    expl. constant in progress

--- src/explanation_of_constants.R
+++ src/explanation_of_constants.R
@@ -1,12 +1,30 @@
+
+# miscallenous constants
+
 sideralyear = 365*24*3600 + 6*3600 + 9*60 + 10
-tropcialyear = 365.2422*24*60*60
-
+tropicalyear = 365.2422*24*60*60
 n=2*pi/sideralyear
-omega=2*pi/(23*60*60+56*60+04)
-omega=2*pi/(24*60*60)
-CAC = 0.003267
+# n=2*pi/tropicalyear
+CAC_ref <- 0.003267
 
-apo = n*n/omega*CAC*1.5*(1-0.017^2)^(-1.5)
+
+alpha_ref = 1/(81.30)/(332958)*(149600e3/384400)^3.
+c_ref = 5.15*pi/180  # inclination of lunar orbit
+
+# two definitions for omega: one for sideral day and one for solar day 
+
+omega_sideral=2*pi/(23*60*60+56*60+04)
+omega_solar=2*pi/(24*60*60)
+
+# apo is the variable called P0 in Berger's thesis  and
+# is referred to as a Newcomb constant
+# 
+# 
+
+apo <- function(CAC=CAC_ref) {
+  apo <- n*n/omega_solar*CAC*1.5*(1-0.017^2)^(-1.5)
+  apo 
+}
 
 # i find his value for e_ref = 0.044. 
 # another possibility, to be very close to the value he has, is 
@@ -14,20 +32,34 @@ apo = n*n/omega*CAC*1.5*(1-0.017^2)^(-1.5)
 # not entirely clear which one to use. For me it should be the sideral day. 
 # so there may be an error, here
 
-apo*60*60* sideralyear *(180/pi) 
-apo*60*60* tropicalyear *(180/pi) 
+ala <- function(alpha = alpha_ref, eref = 0.017, ece=0.055, c=c_ref) {
 
-alpha = 1/(81.30)/(332958)*(149600e3/384400)^3.
-eref = 0.017
-ece = 0.055
+  # eref = 0.017
+  # ece = 0.055
+ 
 
-ala = 1.5*n*n/omega * CAC * (alpha*(1-eref^2)^(-1.5) + (1-ece^2)^(-1.5))
+  # this formula is  the formula for "l_0"  that comes 
+  # from his formula for "l"  given en p. 112
+  # it uses the fact that on an ellipse  
+  # 
+  ala <-  1.5*n*n/omega_sideral * CAC * (alpha*(1-eref^2)^(-1.5) + (1-ece^2)^(-1.5))
+  
+  moonfactor = 1-1.5*sin(c)*sin(c)
+  ala <- ala*60*60* sideralyear *(180/pi)  * moonfactor
+  ala
+}
 
-c = 5.15*pi/180  # inclination of lunar orbit
+# tests
 
-moonfactor = 1-1.5*sin(c)*sin(c)
+print ('apo : Berger reference is 17.3919')
+print ( apo()*60*60* sideralyear *(180/pi) ) 
+print (apo()*60*60* tropicalyear *(180/pi) )
 
-ala*60*60* sideralyear *(180/pi)  * moonfactor
+print ('ala : Berger reference is 54.9066')
+print ( ala() )
+
+
+
 
 # je trouve ici 54.67759 au lieu de ses 54.9066. Environ 0.5% de difference.
 
