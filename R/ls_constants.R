ddc0ac54f9a1d9d77b37c26a8de2a986bc04d6db
mcrucifix <michel.crucifix@uclouvain.be>
2024-03-21 14:52:25 +0100

    newcomb params

--- R/ls_constants.R
+++ R/ls_constants.R
@@ -23,7 +23,7 @@ ece_ref <- 0.055  # moon orbit excentricity
 
 # alpha p. 112 est ml/mtheta * (atheta_al)^3
 
-alpha_ref = 1/(81.30)/(332958)*(149600e3/384400)^3.
+earth_moon_distance_ref = 384400
 c_ref = 5.15*pi/180  # inclination of lunar orbit
 
 # two definitions for omega: one for sideral day and one for solar day 
@@ -35,8 +35,7 @@ omega_solar=2*pi/(24*60*60)
 # apo is the variable called P0 in Berger's thesis  and
 # is referred to as a Newcomb constant
 # 
-# alpha is the mass ratio multiplied by the semi-great axes ratios of moon and earth. It is 
-#       considered to be constant. 
+# alpha is the mass ratio multiplied by the semi-great axes ratios of moon and earth. 
 # 
 
 # - this is the public handle 
@@ -52,19 +51,30 @@ omega_solar=2*pi/(24*60*60)
 #' d(ell)/d(e^2) = 3/2 P0, where "e" is the obliquity. All parameters are given default values
 #' coresponding to the present-day state. 
 #' 
-#' @param CAC is (C-A)/C where A and C are Earth's moments of inertia aroud the plora and equatioral axis of inertia
-#' @param omega is the rotational angular velocity of the Earth, measured in a geocentric framework
-#' @param c is the inclination of the  lunar orbit on the ecliptic
-#' @param alpha is the ratio of moon over earth's masses, multiplied by the ration of earth over moon's semi-major axis, at power 3. It is generally considered to be constant 
-#' @return a list with "ell" and "P0", ready for use in `compute_tables`
+#' @param CAC is (C-A)/C where A and C are Earth's moments of inertia aroud the plora and equatioral axis of inertia. Non-dimensional
+#' @param omega is the rotational angular velocity of the Earth, measured in a geocentric framework, in radians per second
+#' @param c is the inclination of the  lunar orbit on the ecliptic, in radians
+#' @param earth_moon_distance is expressed in km
+#' @return a list with "ell" and "P0", ready for use in `compute_tables`, in arc seconds per year. 
 #' @author Michel Crucifix, UC Louvain, Belgium
 #' @references Berger, A. L. (1973)
 #' Berger A. L. (1973), The astronomical theory of paleoclimates, Theses, UCLouvain, 1973. Currently not available in digital version. 
 #' Berger A., M. Loutre and V. Dehant (1989), Astronomical frequencies for pre-Quaternary palaeoclimate studies, Terra Nova, (1) 474–479 doi:10.1111/j.1365-3121.1989.tb00413.x
+#' @examples
+#' # we consider the AstroGeo model of Farhat et al., Astron. Asttroph. 665
+#' # length of day at -370 Ma : 21.81
+#' # semi-major axis of moon: 361000km
+#' N = newcomb_parameters (omega = 2*pi/(21.81*3600), earth_moon_distance = 361000)
+#' print (N)
+#' # assuming, still according to the AstroGeo22 model, an obliquity of 22.2 degrees, 
+#' # this simple model suggests a precession rate of
+#' N$ala * cos(22.2 * pi/180)
+#' MORE WORK IS NEEDED HERE BECAUSE  C-A/C also varies with OMEGA
+#'
 #' @export newcomb_parameters
-newcomb_parameters <- function(CAC=CAC_ref, omega = omega_solar, c=c_ref, ece = ece_ref, alpha=alpha_ref){
+newcomb_parameters <- function(CAC=CAC_ref, omega = omega_solar, c=c_ref, ece = ece_ref, earth_moon_distance = earth_moon_distance_ref){
   apo_parameter <- ala(CAC, 0., omega)
-  ala_parameter <- ala(CAC, alpha, 0., ece, c, omega)
+  ala_parameter <- ala(CAC, earth_moon_distance,  0., ece, c, omega)
   return (list(ala = ala_parameter, apo = apo_parameter))
 }
 
@@ -81,7 +91,7 @@ apo <- function(CAC=CAC_ref, e=e_ref, omega = omega_solar) {
 # not entirely clear which one to use. For me it should be the sideral day. 
 # so there may be an error, here
 
-ala <- function(CAC=CAC_ref, alpha = alpha_ref, e = e_ref, ece=ece_ref , c=c_ref, omega = omega_solar){
+ala <- function(CAC=CAC_ref, earth_moon_distance = earth_moon_distance_ref, e = e_ref, ece=ece_ref , c=c_ref, omega = omega_solar){
   # e_ref = 0.017
   # ece = 0.055
   # I am not sure these are the values used by Berger in his thesis
@@ -94,6 +104,8 @@ ala <- function(CAC=CAC_ref, alpha = alpha_ref, e = e_ref, ece=ece_ref , c=c_ref
   # see the attached maxima nootebok in scientific doc
   # it comes out that, p. 112, d\ell/de^2 = -3/2*a*(-5/2)
   # 
+
+  alpha = 1/(81.30)/(332958)*(149600e3/earth_moon_distance)^3.
   moonfactor = 1-1.5*sin(c)*sin(c)
   ala <-  1.5*n*n/omega * CAC * (alpha*(1-ece^2)^(-1.5)*moonfactor + (1-e^2)^(-1.5) )
   ala <- ala*60*60* tropicalyear *(180/pi)
