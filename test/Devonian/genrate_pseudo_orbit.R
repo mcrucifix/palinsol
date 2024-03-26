83f4bafbfbb0c28850d9cbd12f996043524681ca
mcrucifix <michel.crucifix@uclouvain.be>
2024-03-26 14:53:47 +0100

    ready for loic 1st attempt

--- test/Devonian/genrate_pseudo_orbit.R
+++ test/Devonian/genrate_pseudo_orbit.R
@@ -1,5 +1,10 @@
+# load_all () 
+install_github("mcrucifix/palinsol@deeppast_test")
+
+
+require(palinsol)
+
 times=seq(-10e3,5)*1e3
-pregas <- seq(50,62,0.25)
 
 # generate frequencies
 
@@ -10,47 +15,42 @@ pregas <- seq(50,62,0.25)
 #                p2= Table2[3,5], 
 #                p3= Table1[1,5]))})
 # 
-DevonTables  <- compute_tables(prea=60.250000)
-ModernTables <- compute_tables(prea=50.273147)
+
+# data from AstroGeo22 for -300 M
+# http://www.astrogeo.eu/wp-content/uploads/2022/11/AstroGeo22.txta
+
+ # 0.370   57.673582   57.335846   57.885557   21.818198   21.547225   21.991389   22.231292   22.227687   22.236342     61.062980     60.584835     61.826758
+
+ estar <- 21.818198 
+ prma <- 61.062980    
+ ala <- prma * cos(estar * pi / 180)
+ 
+
+ # this is still code in development. bea=0 to tell palinsol not to
+ # take into account initial conditions
+DevonTables  <- compute_tables(bea=0, ala=ala, ha = estar, prma = prma)
+
+ # you can force estar to be the value supplied
+
+DevonTables$estar <- estar
+
+# print the main precession frequencies
+
+DevonTables$Table2[seq(10),]
+
+# print the main obliquity frequencies
 
 
-plot ( ModernTables$Table2[,3] - DevonTables$Table2[,3])
+DevonTables$Table1[seq(10),]
+
+# compare with the standard BER90
+
+BER90$Table2[seq(10),]
+
+# compute a solution
 
  o1 <- function(t)  table_based_solution(t, DevonTables)
- o2 <- function(t)  table_based_solution(t, ModernTables)
-
- DevonExample <- sapply(times,o1)
- ModernExample <- sapply(times,o2)
-
-
-ModernTables <- ModernTables[seq(12700),]
-DevonTables <- DevonTables[seq(12700),]
-
-O1 <- order(ModernTables$Table2[,5])
-O2 <- order(DevonTables$Table2[,5])
-
-# ModernSpectrum <- ModernTables$Table2[O1,c(5,2)]
-# DevonSpecturm <- DevonTables$Table2[O2,c(5,2)]
-
-pdf('modern_vs_devonian_1.pdf')
-
-plot(times, ModernExample['esinw',], type='l');
-lines(times, ModernExample['ecc',], type='l', lty=2);
-lines(times, DevonExample['esinw',], type='l', col='red');
-lines(times, DevonExample['ecc',], type='l', col='red');
-
-legend('topright',c('modern','Devon'), col=c('black','red'))
-
-dev.off()
-
-pdf('modern_vs_devonian_2.pdf')
-
-plot(times, ModernExample['esinw',], type='l', xlim=c(-500000,0));
-lines(times, ModernExample['ecc',], type='l', lty=2);
-lines(times, DevonExample['esinw',], type='l', col='red');
-lines(times, DevonExample['ecc',], type='l', col='red');
-
-legend('topright',c('modern','Devon'), col=c('black','red'))
+ time_solution <- sapply ( times, o1)
 
 dev.off()
 
