3e5d44ab61b5115dcd43c2843e5b68ffb4be1596
mcrucifix <michel.crucifix@uclouvain.be>
2025-02-19 17:22:10 +0100

    updated eccentricity spectrum + doc cleanup

--- R/eccentricity_spectrum.R
+++ R/eccentricity_spectrum.R
@@ -1,27 +1,11 @@
-require(gtools)
+# thi actually provides the eccentricity spectrum of 
+# m(1+A/2-A^2/8+A^3/16) where A= e^2/m^2 - 1 , and
+# the definition of m given below. 
+# this confirms the accuracy of the development given in Berger an Loutre 1990 
+# and the fact that the square root has been developed up to the third order in (1-e^2/m). 
+# errors are thus of the order of 5/128A^4, which perhaps surprisingly is still of the order of 0.015 / 0.020 in eccentricity. Errors are seem to be always positive (5/128*A^4 may reach 2.0, as A may reach 2.0, but not easy to prove because the next term, in A^5, can be large as well. 
 
-
-combine <- function(a,b, om1, om2, ph1, ph2, factor=1) {
-  A <- factor * outer(a,b, "*")
-  O <- outer(om1,om2, "+")
-  P <- outer(ph1,ph2, "+")
-  A <- A[upper.tri(A)]
-  O <- O[upper.tri(O)]
-  P <- P[upper.tri(P)]
-  return(data.frame(A=A,O=O,P=P))
-}
-
-
-# might be a faster alternative but not used here in the end
-combine2 <- function(a,b, om1, om2, ph1, ph2) {
-IJ = gtools::combinations(length(a),2)
-  A <- apply(IJ, 1, function(V) a[V[1]]*b[V[2]])
-  O <- apply(IJ, 1, function(V) om1[V[1]]+om2[V[2]])
-  P <- apply(IJ, 1, function(V) ph1[V[1]]+ph2[V[2]])
-  return(data.frame(A=A,O=O,P=P))
-}
-
-
+# in fact using higher order terms helps a bit for capturing the 'low eccentricity' bit, but it actually makes things much worse above 0.04, so that the third order is actually the right deal. 
 
 
 #' Trigonometrical development of eccentricity spectrum, based on an erbital solution
@@ -36,30 +20,72 @@ IJ = gtools::combinations(length(a),2)
 #' @references Berger A. and M. F. Loutre (1990), Origine des frequences des elements astronomiques intervenant dans le calcul de l'insolation, Bulletin de la Classe des sciences, (1) 45-106 doi:10.3406/barb.1990.38523
 #' @importFrom gtools combinations
 #' @note # this actually provides the (exact) spectrum of m(1+A/2-A^2/8+A/16) where A= e^2/m^2 - 1 , and m =  sqrt(sum(epi$Amp^2))
-#'
-eccentricity_spectrum <- function(epi, inputfreq = 10){
+#
+eccentricity_spectrum <- function(G) {
 
-# O1 <- order(epi$Freq, decreasing=TRUE)[seq(inputfreq)]
-O1 <- seq(inputfreq)
+require(gtools)
 
-epi <- list(Freq=epi$Freq[O1] / 1000, Amp=epi$Amp[O1], Phases=epi$Phases[O1])
+# the file EPI containts the development of "e sin Pi" which is the 
+# only file needed to produce the development of eccentricity
+# This particular file comes from La93.
 
-m <- sqrt(sum(epi$Amp^2))
-a = epi$Amp / m
+## Il y asans doute une erreur dans la page 54 de Berger et Loutre, mais ou ???
+
+## Revoir la these de Berger ? 
+
+print('init tables')
+
+EPI <- G
+
+EPI$omega <- as.data.frame(G)[, 'Freq']
+EPI$M <- as.data.frame(G)[, 'Amp']
+EPI$phi <- as.data.frame(G)[, 'Phases']
+
+
+
+combine <- function(a,b, om1, om2, ph1, ph2, factor=1) {
+  A <- factor * outer(a,b, "*")
+  O <- outer(om1,om2, "+")
+  P <- outer(ph1,ph2, "+")
+  A <- A[upper.tri(A)]
+  O <- O[upper.tri(O)]
+  P <- P[upper.tri(P)]
+  return(data.frame(A=A,O=O,P=P))
+}
+
+combine2 <- function(a,b, om1, om2, ph1, ph2) {
+IJ = gtools::combinations(length(a),2)
+  A <- apply(IJ, 1, function(V) a[V[1]]*b[V[2]])
+  O <- apply(IJ, 1, function(V) om1[V[1]]+om2[V[2]])
+  P <- apply(IJ, 1, function(V) ph1[V[1]]+ph2[V[2]])
+  return(data.frame(A=A,O=O,P=P))
+}
+
+
+
+
+# order  by decreasing frequency
+
+O1 <- order(EPI$omega, decreasing=TRUE)
+
+EPI <- list(omega=EPI$omega[O1], M=EPI$M[O1], phi=EPI$phi[O1])
+
+m <- sqrt(sum(EPI$M^2))
+a = EPI$M / m
 
 
 n=0
 
 
-B <- combine(a,a, epi$Freq, -epi$Freq,  epi$Phases, -epi$Phases)
+B <- combine(a,a, EPI$omega, -EPI$omega,  EPI$phi, -EPI$phi)
 
 bk <- B$A
 gk <- B$O
 pk <- B$P
 
-# bk <- epi$Amp[IJB[,1]] * epi$Amp[IJB[,2]]
-# gk <- epi$Freq[IJB[,1]] - epi$Freq[IJB[,2]]
-# pk <- epi$Phases[IJB[,1]] - epi$Phases[IJB[,2]]
+# bk <- EPI$M[IJB[,1]] * EPI$M[IJB[,2]]
+# gk <- EPI$omega[IJB[,1]] - EPI$omega[IJB[,2]]
+# pk <- EPI$phi[IJB[,1]] - EPI$phi[IJB[,2]]
 
 ek1 <- bk 
 ek2 <- 0.375*bk*bk*bk 
@@ -167,34 +193,34 @@ print('ecremage')
 
 #group <- lapply(group, function(developpement) developpement[which(abs(developpement$A) > 1.e-6),])
 
-## print ('generate partial reconstructions')
-## 
-## times <- seq(0,5e3,2)*1e3
-## X <- t(sapply(group, function(developpement)
-##    sapply(times, function(t) sum(m * developpement$A * cos(developpement$O*t+developpement$P))) ))
-## 
-## esinpi <- sapply(times, function(t) sum(epi$Amp * sin(epi$Freq*t+epi$Phases)))
-## ecospi <- sapply(times, function(t) sum(epi$Amp * cos(epi$Freq*t+epi$Phases)))
-## Y = sqrt(esinpi*esinpi+ecospi*ecospi)
-## 
-## 
-## 
-## XX <- apply(X,2,sum)
-## 
-## Xts <- ts(XX, deltat=times[2]-times[1])
-## Yts <- ts(Y, deltat=times[2]-times[1])
-## 
-## 
-## # thi actually provides the eccentricity spectrum of 
-## # m(1+A/2-A^2/8+A/16) where A= e^2/m^2 - 1 , and
-## # the definition of m given above
-## 
-## A = (Yts/m)^2 - 1;
-## Yts2 <- m*(1 + A/2 - A*A/8 + A*A*A/16)
-## 
-## Yts3 <- m*(1 + A/2 - A*A/8 + A*A*A/16 - 5*A^4/128)
-## Yts4 <- m*(1 + A/2 - A*A/8 + A*A*A/16 - 5*A^4/128 + 7*A^5/256)
-## 
+print ('generate partial reconstructions')
+
+times <- seq(0,5e3,2)*1e3
+X <- t(sapply(group, function(developpement)
+   sapply(times, function(t) sum(m * developpement$A * cos(developpement$O*t+developpement$P))) ))
+
+esinpi <- sapply(times, function(t) sum(EPI$M * sin(EPI$omega*t+EPI$phi)))
+ecospi <- sapply(times, function(t) sum(EPI$M * cos(EPI$omega*t+EPI$phi)))
+Y = sqrt(esinpi*esinpi+ecospi*ecospi)
+
+
+
+XX <- apply(X,2,sum)
+
+Xts <- ts(XX, deltat=times[2]-times[1])
+Yts <- ts(Y, deltat=times[2]-times[1])
+
+
+# thi actually provides the eccentricity spectrum of 
+# m(1+A/2-A^2/8+A/16) where A= e^2/m^2 - 1 , and
+# the definition of m given above
+
+A = (Yts/m)^2 - 1;
+Yts2 <- m*(1 + A/2 - A*A/8 + A*A*A/16)
+
+Yts3 <- m*(1 + A/2 - A*A/8 + A*A*A/16 - 5*A^4/128)
+Yts4 <- m*(1 + A/2 - A*A/8 + A*A*A/16 - 5*A^4/128 + 7*A^5/256)
+
 # now you can check that Xts and Yts2 match perfectly, to 1e-16
 
 
@@ -213,15 +239,70 @@ AFF <- cbind(AFF1, P=AFF2$P)
 
 
 print ('liste finale:')
-names(AFF) <- c("Freq","Amp","Phases")
 
-AFF$Amp <- AFF$Amp * m
-AFF$Freq <- AFF$Freq * 1000. 
+negative_amplitudes <- which(AFF$A < 0)
 
-class(AFF) <- "discreteSpectrum"
+AFF$A[negative_amplitudes] <-      - AFF$A[negative_amplitudes]
+AFF$O[negative_amplitudes] <-      - AFF$O[negative_amplitudes]
+AFF$P[negative_amplitudes] <-   pi - AFF$P[negative_amplitudes]
 
+
+o = order(AFF$A, decreasing=TRUE)
+
+
+
+out <- list (Freq= AFF$O[o], Amp = AFF$A[o]*m, Phases = AFF$P[o])
+class(out) <- "discreteSpectrum"
+
+return(out)
+}
 #print(length(AFF$A))
 
-return(AFF)
-}
-## 
+
+# AFF$A = AFF$A*m
+# developpement$A = developpement$A*m
+
+
+##
+
+#### print('ici')
+#### print(length(AFF$A))
+#### 
+#### print('reconstruction...')
+#Xts2 <- ts (  sapply(times, function(t) sum(AFF$A * cos(AFF$O*t+AFF$P))), deltat = times[2]-times[1]) * m
+
+
+
+# Xts and Xts2 also match perfectly 
+
+
+
+
+#### print('palinsol.')
+#### ecc_ber90 <- sapply(times, ber90)['ecc',]
+#### 
+#### 
+#### 
+#### 
+#### 
+#### plot(ecc_reconstruct, type='l')
+#### # lines(ecc_reconstruct_non_aggregated, type='l', col='blue')
+#### lines(ecc_ber90-0.02, col='red')
+#### 
+#### plot(ecc_reconstruct - ecc_ber90, type='l')
+#### 
+#### print('true variances')
+#### # print(var(ecc_reconstruct_non_aggregated))
+#### print(var(ecc_reconstruct))
+#### print(var(ecc_ber90))
+#### print(var(ecc_ber90 - ecc_reconstruct))
+#### 
+#### # Order1 <- order(AFF$O, decreasing=FALSE)
+#### # Order2 <- order(AFF$A, decreasing=TRUE)
+#### # 
+#### # plot(2*pi/AFF$O[Order1], AFF$A[Order1], type='l', xlim=c(0,3e6))
+#### # AFF$period = 2*pi/AFF$O
+#### # 
+#### # AFF[Order2[seq(10)],]
+#### # 
+#### 
